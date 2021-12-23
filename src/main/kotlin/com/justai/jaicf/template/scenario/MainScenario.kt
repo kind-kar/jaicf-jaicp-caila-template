f9b89baab5a3e73855ab276ae3077fd17ef7ea35
kind-kar <61771806+kind-kar@users.noreply.github.com>
2021-12-23 16:15:42 +0300

    Update MainScenario.kt
    add commit for regress

--- src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
+++ src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
@@ -14,7 +14,8 @@ val mainScenario = Scenario {
                 image("https://media.giphy.com/media/ICOgUNjpvO0PC/source.gif")
                 sayRandom(
                     "Hello! How can I help?",
-                    "Hi there! How can I help you?"
+                    "Hi there! How can I help you?",
+                    "Hi, I dont speak english"
                 )
                 buttons(
                     "Help me!",
